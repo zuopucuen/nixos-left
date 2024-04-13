0ff5ad9c4ba83d10b1ddf97e05a85ff0a5596cc3
left <zuopucuen@qq.com>
2024-04-13 08:39:45 +0800

    update sddm option

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -4,14 +4,14 @@
   services = {
     dbus.enable = true;
     desktopManager.plasma6.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
     xserver = {
       enable = true;
       #dpi = 192;
       #upscaleDefaultCursor = true;
-      displayManager.sddm = {
-        enable = true;
-        wayland.enable = true;
-      };
     };
   };
 
