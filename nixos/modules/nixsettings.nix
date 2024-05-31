1ef165ad80624a4f9a283d5d30d837c98c7a332e
left_left <zuopucuen@qq.com>
2024-05-31 23:12:34 +0800

    Update nixsettings.nix
    change mirrors to https://mirrors.ustc.edu.cn/nix-channels/

--- nixos/modules/nixsettings.nix
+++ nixos/modules/nixsettings.nix
@@ -11,12 +11,8 @@
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
       substituters = lib.mkForce [
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
-        #  "https://nix-gaming.cachix.org"
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
       ];
-      #trusted-public-keys = [
-      #  "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      #];
     };
 
     gc = {
