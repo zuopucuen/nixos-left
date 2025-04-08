19dee2a103c2b14499d8199f8c40a772356e47b9
left <zuopucuen@qq.com>
2025-04-08 20:29:52 +0800

    add default fonts

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -11,4 +11,27 @@
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
+  fonts.fontconfig= {
+    defaultFonts = {
+      emoji = [
+        "Noto Color Emoji"
+      ];
+      monospace = [
+        "Noto Sans Mono CJK SC"
+        "Sarasa Mono SC"
+        "DejaVu Sans Mono"
+      ];
+      sansSerif = [
+        "Noto Sans CJK SC"
+        "Source Han Sans SC"
+        "DejaVu Sans"
+      ];
+      serif = [
+        "Noto Serif CJK SC"
+        "Source Han Serif SC"
+        "DejaVu Serif"
+      ];
+    };
+  };
+
 }
